988734e04c2d6052fca6efde7e1a81178d03ca67
Gihan-1994 <nggp1994@gmail.com>
2025-01-10 06:24:07 +0530

    hi

--- todolist/src/components/TodoList.tsx
+++ todolist/src/components/TodoList.tsx
@@ -6,17 +6,46 @@ import { GiCancel } from 'react-icons/gi'
 import { RiDeleteBin5Fill } from 'react-icons/ri'
 import TodoForm from './TodoForm'
 import '../CSS/TodoList.css'
+import Checkbox from '@mui/material/Checkbox';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const TodoList = () => {
     const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos());
     const [edithingTodoId, setEditedTodoId] = useState<number | null>(null);
     const [editedTodoText, setEditedTodoText] = useState<string>("");
+   // const [checkCompleted,setCheckCompleted] = useState<boolean>(false)
+  
 
 
     const handleTodoStart = (id: number, text: string) => {
         setEditedTodoId(id);
         setEditedTodoText(text);
     }
+    const handleCheckChange = (id: number) => {
+      const getCheckedTodo = todos.find((todo)=>todo.id===id)
+      if(getCheckedTodo){
+      const updateCheckedTodo = TodoService.updateTodo(
+        {
+           id :id,
+           text :getCheckedTodo.text,
+           completed : !getCheckedTodo.completed 
+        }
+      )
+      setTodos((prvTodos) => prvTodos.map((todo) => (todo.id === id ? updateCheckedTodo : todo)))
+    }
+
+    }
+
+    // const handleCheckChange = (id: number) => {
+    
+    //     const updatedTodo = todos.find((todo) => todo.id === id); 
+    // if (updatedTodo) {      
+    //   updatedTodo.completed = !updatedTodo.completed ;
+    //   TodoService.updateTodo(updatedTodo); 
+    //   setTodos([...todos])
+    //     console.log(todos)
+    //   }};
 
     const handleTodoCancel = () => {
         setEditedTodoId(null);
@@ -79,6 +108,11 @@ const TodoList = () => {
                         <button onClick={() => handleDeleteTodo(todo.id)}>
                             <RiDeleteBin5Fill />
                         </button>
+                        <div className="items" >
+  
+  <Checkbox {...label} checked={todo.completed} onChange={() => handleCheckChange(todo.id)} color="success" />
+
+</div>
                     </div>
                 ))}
             </div>
